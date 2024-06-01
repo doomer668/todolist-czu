10ea3bd15a61d059699c2cd0e0a86d4e3af46cfa
doomer668 <boldamir0@gmail.com>
2024-06-01 15:45:40 +0300

    add project

---
+++ TodoList/ToDo List App/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace ToDo_List_App
+{
+	partial class ToDoList
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.titleTextBox = new System.Windows.Forms.TextBox();
+			this.descriptionTextBox = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.newButton = new System.Windows.Forms.Button();
+			this.editButton = new System.Windows.Forms.Button();
+			this.deleteButton = new System.Windows.Forms.Button();
+			this.saveButton = new System.Windows.Forms.Button();
+			this.toDoListView = new System.Windows.Forms.DataGridView();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			((System.ComponentModel.ISupportInitialize)(this.toDoListView)).BeginInit();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.Font = new System.Drawing.Font("Roboto Slab", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(587, 83);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "To Do List";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// titleTextBox
+			// 
+			this.titleTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.titleTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.titleTextBox.Location = new System.Drawing.Point(11, 117);
+			this.titleTextBox.MinimumSize = new System.Drawing.Size(588, 20);
+			this.titleTextBox.Name = "titleTextBox";
+			this.titleTextBox.Size = new System.Drawing.Size(588, 20);
+			this.titleTextBox.TabIndex = 1;
+			// 
+			// descriptionTextBox
+			// 
+			this.descriptionTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.descriptionTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.descriptionTextBox.Location = new System.Drawing.Point(11, 173);
+			this.descriptionTextBox.MinimumSize = new System.Drawing.Size(588, 20);
+			this.descriptionTextBox.Name = "descriptionTextBox";
+			this.descriptionTextBox.Size = new System.Drawing.Size(588, 20);
+			this.descriptionTextBox.TabIndex = 2;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Roboto Slab", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.Location = new System.Drawing.Point(7, 93);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(48, 21);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Title:";
+			this.label2.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Roboto Slab", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label3.Location = new System.Drawing.Point(7, 149);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(102, 21);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Description:";
+			this.label3.Click += new System.EventHandler(this.label3_Click);
+			// 
+			// newButton
+			// 
+			this.newButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.newButton.Font = new System.Drawing.Font("Roboto Slab", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.newButton.Location = new System.Drawing.Point(3, 3);
+			this.newButton.MinimumSize = new System.Drawing.Size(141, 49);
+			this.newButton.Name = "newButton";
+			this.newButton.Size = new System.Drawing.Size(141, 49);
+			this.newButton.TabIndex = 5;
+			this.newButton.Text = "New";
+			this.newButton.UseVisualStyleBackColor = true;
+			// 
+			// editButton
+			// 
+			this.editButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.editButton.Font = new System.Drawing.Font("Roboto Slab", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.editButton.Location = new System.Drawing.Point(150, 3);
+			this.editButton.MinimumSize = new System.Drawing.Size(141, 49);
+			this.editButton.Name = "editButton";
+			this.editButton.Size = new System.Drawing.Size(141, 49);
+			this.editButton.TabIndex = 6;
+			this.editButton.Text = "Edit";
+			this.editButton.UseVisualStyleBackColor = true;
+			this.editButton.Click += new System.EventHandler(this.editButton_Click_1);
+			// 
+			// deleteButton
+			// 
+			this.deleteButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.deleteButton.Font = new System.Drawing.Font("Roboto Slab", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.deleteButton.Location = new System.Drawing.Point(297, 3);
+			this.deleteButton.MinimumSize = new System.Drawing.Size(141, 49);
+			this.deleteButton.Name = "deleteButton";
+			this.deleteButton.Size = new System.Drawing.Size(141, 49);
+			this.deleteButton.TabIndex = 7;
+			this.deleteButton.Text = "Delete";
+			this.deleteButton.UseVisualStyleBackColor = true;
+			this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+			// 
+			// saveButton
+			// 
+			this.saveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.saveButton.Font = new System.Drawing.Font("Roboto Slab", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.saveButton.Location = new System.Drawing.Point(444, 3);
+			this.saveButton.MinimumSize = new System.Drawing.Size(141, 49);
+			this.saveButton.Name = "saveButton";
+			this.saveButton.Size = new System.Drawing.Size(141, 49);
+			this.saveButton.TabIndex = 8;
+			this.saveButton.Text = "Save";
+			this.saveButton.UseVisualStyleBackColor = true;
+			this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+			// 
+			// toDoListView
+			// 
+			this.toDoListView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.toDoListView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.toDoListView.BackgroundColor = System.Drawing.Color.LightGray;
+			this.toDoListView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.toDoListView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.toDoListView.Location = new System.Drawing.Point(12, 259);
+			this.toDoListView.MinimumSize = new System.Drawing.Size(587, 333);
+			this.toDoListView.Name = "toDoListView";
+			this.toDoListView.Size = new System.Drawing.Size(587, 333);
+			this.toDoListView.TabIndex = 9;
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.tableLayoutPanel1.ColumnCount = 4;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel1.Controls.Add(this.editButton, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.saveButton, 3, 0);
+			this.tableLayoutPanel1.Controls.Add(this.deleteButton, 2, 0);
+			this.tableLayoutPanel1.Controls.Add(this.newButton, 0, 0);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(11, 199);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 1;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(588, 55);
+			this.tableLayoutPanel1.TabIndex = 10;
+			// 
+			// ToDoList
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ButtonFace;
+			this.ClientSize = new System.Drawing.Size(612, 618);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Controls.Add(this.toDoListView);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.descriptionTextBox);
+			this.Controls.Add(this.titleTextBox);
+			this.Controls.Add(this.label1);
+			this.MinimumSize = new System.Drawing.Size(628, 643);
+			this.Name = "ToDoList";
+			this.Text = "To Do List";
+			this.Load += new System.EventHandler(this.ToDoList_Load);
+			((System.ComponentModel.ISupportInitialize)(this.toDoListView)).EndInit();
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox titleTextBox;
+		private System.Windows.Forms.TextBox descriptionTextBox;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button newButton;
+		private System.Windows.Forms.Button editButton;
+		private System.Windows.Forms.Button deleteButton;
+		private System.Windows.Forms.Button saveButton;
+		private System.Windows.Forms.DataGridView toDoListView;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+	}
+}
+
